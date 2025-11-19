5aa9fbf8a05b3ff686af8cf95d46c1eb1ac0e089
Yoshikawa Project <ilogs.lya@gmail.com>
2025-11-19 13:35:13 -0300

    Update generate-banner-v2.js

--- api/generate-banner-v2.js
+++ api/generate-banner-v2.js
@@ -2,7 +2,7 @@ import { createCanvas, GlobalFonts, loadImage } from '@napi-rs/canvas';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Fontes
+// Fontesu
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
