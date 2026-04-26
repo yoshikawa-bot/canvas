7fbcf8643876b0bb75efec477cf9d4f93a9322b0
Yoshikawa Project <ilogs.lya@gmail.com>
2026-04-26 16:50:22 -0300

    Update avatar image URL in playmg.js

--- api/playmg.js
+++ api/playmg.js
@@ -177,7 +177,7 @@ export default async function handler(req, res) {
 
     let avatarImg = null;
     try {
-        const response = await fetch('https://yoshikawa-bot.github.io/cache/images/8c5603b3.jpg');
+        const response = await fetch('https://yoshikawa-bot.github.io/cache/images/b9c703b8.jpg');
         const buf = Buffer.from(await response.arrayBuffer());
         avatarImg = await loadImage(buf);
     } catch (e) { }
